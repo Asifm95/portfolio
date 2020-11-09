660638c2077a0db2d97babfaa26325c5568558e0
Asif Mohammed <asifm2882@gmail.com>
2020-11-09 14:43:38 +0400

    setup app container

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Nav from './Nav.svelte';
+    import Hero from './Hero.svelte';
+</script>
+
+<style lang="scss">
+    main {
+        width: 100%;
+        height: 100%;
+    }
+</style>
+
+<main>
+    <Nav />
+    <Hero />
+</main>
