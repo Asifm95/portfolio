c83c61712e29d4bfe84ce1629d1ec5288d6cf969
Asif Mohammed <asifm2882@gmail.com>
2020-11-09 20:44:03 +0400

    move static data to model

---
+++ src/model/links.js
@@ -0,0 +1,6 @@
+export const links = [
+    { id: 1, ref: '#about', label: 'about' },
+    { id: 2, ref: '#showcase', label: 'Showcase' },
+    { id: 3, ref: '#contact', label: 'Contact' },
+    { id: 4, ref: 'https://github.com/Asifm95', label: 'Asifm95' },
+];
