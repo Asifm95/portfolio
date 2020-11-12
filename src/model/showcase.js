5580c1b8e78c593339ed813788ebc96bc9a2e8cc
Asif Mohammed <asifm2882@gmail.com>
2020-11-12 20:29:59 +0400

    add showcase model

---
+++ src/model/showcase.js
@@ -0,0 +1,28 @@
+export default [
+    {
+        id: 1,
+        link: 'https://www.blankwear.com/',
+        title: 'Blankwear',
+        content: 'A wholesale blank apparel E-commerce platform.',
+    },
+    {
+        id: 2,
+        link: 'https://storybrain.io/',
+        title: 'Storybrain',
+        content:
+            'Improve user engagement by evolving content with the help of AI.',
+    },
+    {
+        id: 3,
+        link: 'https://asifm95.github.io/react-spaces-ui/',
+        title: 'Spaces UI',
+        content:
+            'Inspired by the Invision Projects - Spaces iOS by @charles Patterson',
+    },
+    {
+        id: 4,
+        link: 'https://asifm95.github.io/react-color-palette',
+        title: 'Color Palette',
+        content: 'A flatuicolors inspired color palettes picker.',
+    },
+];
