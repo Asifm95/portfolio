b8ad2a5148fde7fb76561297750ff418e9d66d57
Asif Mohammed <asifm2882@gmail.com>
2021-02-28 11:02:43 +0400

    update showcase

--- src/model/showcase.js
+++ src/model/showcase.js
@@ -7,22 +7,36 @@ export default [
     },
     {
         id: 2,
+        link:
+            'https://www.blankwear.com/pct/bbf5a156-ebfb-493e-a31d-4108e5812bad/main-menu',
+        title: 'PCT',
+        content: 'An E-commerce product customisation tool.',
+    },
+    {
+        id: 3,
         link: 'https://storybrain.io/',
         title: 'Storybrain',
         content:
             'Improve user engagement by evolving content with the help of AI.',
     },
     {
-        id: 3,
+        id: 4,
+        link: 'https://iprotectu.co.uk/',
+        title: 'IProtectU',
+        content:
+            'EHS software solution to manage risk assessment and H&S compliance.',
+    },
+    {
+        id: 5,
         link: 'https://asifm95.github.io/react-spaces-ui/',
         title: 'Spaces UI',
         content:
             'Inspired by the Invision Projects - Spaces iOS by @charles Patterson',
     },
     {
-        id: 4,
-        link: 'https://asifm95.github.io/react-color-palette',
-        title: 'Color Palette',
-        content: 'A flatuicolors inspired color palettes picker.',
+        id: 6,
+        link: 'https://asifm95.github.io/game-of-life/',
+        title: 'Game of Life',
+        content: "Conway's game of life implemented using React + Hooks.",
     },
 ];
