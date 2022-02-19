5c36a98bf4fb64b6c24da06db97fae954c844fa4
Asif Mohammed <asif@collect.chat>
2022-02-19 09:57:18 +0400

    update showcase

--- src/model/showcase.js
+++ src/model/showcase.js
@@ -1,42 +1,54 @@
 export default [
     {
         id: 1,
-        link: 'https://www.blankwear.com/',
-        title: 'Blankwear',
-        content: 'A wholesale blank apparel E-commerce platform.',
+        link: 'https://whatsform.com/',
+        title: 'Whatsform',
+        content:
+            'A no-code form builder where users can get data directly from customer WhatsApp.',
     },
     {
         id: 2,
-        link:
-            'https://www.blankwear.com/pct/bbf5a156-ebfb-493e-a31d-4108e5812bad/main-menu',
+        link: 'https://workspace.google.com/marketplace/app/form_to_chatbot/349974714221',
+        title: 'Form to Chatbot',
+        content: 'Turn Google form into a highly engaging chat in one click.',
+    },
+    {
+        id: 3,
+        link: 'https://sheetstore.com/',
+        title: 'Sheetstore',
+        content: 'Turn Google sheet into modern, functional online store.',
+    },
+    {
+        id: 4,
+        link: 'https://www.blankwear.com/pct/bbf5a156-ebfb-493e-a31d-4108e5812bad/main-menu',
         title: 'PCT',
         content: 'An E-commerce product customisation tool.',
     },
     {
-        id: 3,
+        id: 5,
         link: 'https://storybrain.io/',
         title: 'Storybrain',
         content:
             'Improve user engagement by evolving content with the help of AI.',
     },
     {
-        id: 4,
+        id: 6,
         link: 'https://iprotectu.co.uk/',
         title: 'IProtectU',
         content:
             'EHS software solution to manage risk assessment and H&S compliance.',
     },
     {
-        id: 5,
+        id: 7,
         link: 'https://asifm95.github.io/react-spaces-ui/',
         title: 'Spaces UI',
         content:
             'Inspired by the Invision Projects - Spaces iOS by @charles Patterson',
     },
-    {
-        id: 6,
-        link: 'https://asifm95.github.io/game-of-life/',
-        title: 'Game of Life',
-        content: "Conway's game of life implemented using React + Hooks.",
-    },
+    // {
+    //     id: 8,
+    //     link: 'https://asifm95.github.io/game-of-life/',
+    //     title: 'Game of Life',
+    //     content: "Conway's game of life implemented using React + Hooks.",
+    // },
 ];
